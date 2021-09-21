d2424b5b6ea6417879563453b19dc2d62229208e
filippo <filippo-ceradini>
2021-09-21 12:35:06 +0200

    Animal zoo

--- src/Run.java
+++ src/Run.java
@@ -2,14 +2,15 @@
 public class Run {
     public static void main(String[] args) {
 
-
+        //Adding the two animals with their attributes
         animal snake = new animal(9, 9, "Snake says: \"Yo dead bunny!\"", "Snake");
         animal bunny = new animal(0, 0, "Rabbit says: \"Oh not again!\"", "Rabbit");
+
+        //Printout animal position at the start
         System.out.println(snake.toAnimalPos());
         System.out.println(bunny.toAnimalPos());
 
-
-
+        //While cycle that makes the animals move until they are in the same cell
         while(bunny.getXpos() != snake.getXpos() && bunny.getYpos() != snake.getYpos()) {
             bunny.moveFwrd();
             if (bunny.getXpos() == snake.getXpos() - 1 && bunny.getYpos() == snake.getYpos() - 1) {
